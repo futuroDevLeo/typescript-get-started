04b088776419968bd1123c8c866f5abdc9693460
futuroDevLeo <leonunes07@outlook.com>
2024-01-01 18:40:09 -0300

    feat: complete challenge of making an object in typescript

---
+++ basico/desafio01.ts
@@ -0,0 +1,41 @@
+// Desafio
+
+// Criar um objeto funcionário com:
+//     - Array de strings com os nomes dos supervisores
+//     - Função de bater ponto que recebe a hora e retorna uma string
+//         -> Ponto normal (<=8)
+//         -> Fora do horário (> 8)
+
+let funcionario: { nome: string, idade: number, aniversario: string, supervisores: string[], ponto: string };
+
+function baterPonto(hora: number): string {
+    if (hora <= 8) {
+        return "Ponto normal"
+    } else {
+        return "Fora do horário"
+    }
+};
+
+let ponto: string = baterPonto(8);
+
+funcionario = {
+    nome: "Pedro",
+    idade: 27,
+    aniversario: "01/02",
+    supervisores: ["João", "Gabriela"],
+    ponto,
+};
+
+console.log(funcionario);
+
+ponto = baterPonto(9);
+
+funcionario = {
+    nome: "Isabela",
+    idade: 29,
+    aniversario: "15/04",
+    supervisores: ["João", "Gabriela"],
+    ponto
+}
+
+console.log(funcionario);
