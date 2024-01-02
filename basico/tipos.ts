92e7fc92275213815fb0599f392fd11f1abf2f9b
futuroDevLeo <leonunes07@outlook.com>
2024-01-02 17:05:27 -0300

    feat: new types added

--- basico/tipos.ts
+++ basico/tipos.ts
@@ -1,24 +1,29 @@
-//string
+// String
 const nome: string = 'Pedro';
 console.log(nome);
 
-//number
+
+// Number
 const idade: number = 27;
 console.log(idade);
 
-//boolean
+
+// Boolean
 const possuiHobbies: boolean = true;
 console.log(possuiHobbies);
 
-//array
+
+// Array
 const hobbies: string[] = ["cozinhar", "esportes"];
 console.log(hobbies);
 
-//tuplas
+
+// Tuplas
 const endereco: [string, number, string] = ["Av. Principal", 123, "Centro"];
 console.log(endereco);
 
-//enums
+
+// Enums
 enum Cor {
     Cinza,
     Verde,
@@ -28,7 +33,8 @@ enum Cor {
 const minhaCor: Cor = Cor.Verde
 console.log(Cor[minhaCor]);
 
-//any
+
+// Any
 let carro: any = "BMW";
 console.log(carro);
 carro = {
@@ -37,7 +43,8 @@ carro = {
 };
 console.log(carro);
 
-//function
+
+// Function
 function multiplicar(numA: number, numB: number): number {
     return numA * numB
 };
@@ -50,14 +57,22 @@ function digaOi(): void {
 
 digaOi();
 
-//tipo function
+
+// Tipo Function
 let calculo: (x: number, y: number) => number
 
 calculo = multiplicar
 console.log(calculo(5, 5));
 
-//objetos
-let usuario: { nome: string, idade: number, endereco: [string, number, string], possuiHobbies: boolean, hobbies: string[] };
+
+// Objetos
+let usuario: {
+    nome: string,
+    idade: number,
+    endereco: [string, number, string],
+    possuiHobbies: boolean,
+    hobbies: string[]
+};
 
 usuario = {
     nome,
@@ -68,3 +83,69 @@ usuario = {
 };
 
 console.log(usuario);
+
+
+// Alias
+type Funcionario = {
+    supervisores: string[],
+    baterPonto: (hora: number) => string
+}
+
+let funcionario: Funcionario = {
+    supervisores: ["Fernanda", "Marcelo"],
+    baterPonto(hora: number): string {
+        if (hora <= 8) {
+            return "Ponto normal"
+        } else {
+            return "Fora do horário"
+        }
+    }
+};
+
+console.log(funcionario.baterPonto(8));
+
+
+// Union Types
+let nota: number | string = 10;
+console.log(nota);
+
+nota = "10";
+console.log(nota);
+
+
+// Never
+function falha(msg: string): never {
+    throw new Error(msg);
+};
+
+const produto = {
+    nome: 'Sabão',
+    preco: 4,
+    validarProduto() {
+        if (!this.nome || this.nome.trim().length == 0) {
+            falha("Precisa ter um nome");
+        }
+        if (this.preco <= 0) {
+            falha("Preço inválido!")
+        }
+    }
+}
+
+produto.validarProduto();
+console.log(produto.nome);
+
+
+// Null
+type Contato = {
+    nome: string,
+    tell1: string,
+    tell2: string | null
+};
+
+const contato: Contato = {
+    nome: "Rafael",
+    tell1: "01 901234567",
+    tell2: null
+};
+
+console.log(contato);
