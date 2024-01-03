ec01310ad302d1ab9e174e28812c1d0499cd4297
futuroDevLeo <leonunes07@outlook.com>
2024-01-03 16:12:53 -0300

    refactor:folders moved from mslearn to this repo to make activities

--- m01/enum.ts
+++
@@ -1,9 +0,0 @@
-enum ContractStatus {
-    Permanent,
-    Temp,
-    Apprentice
-}
-
-let employeeStatus: ContractStatus = ContractStatus.Temp;
-console.log(ContractStatus[employeeStatus]);
-console.log(employeeStatus);
