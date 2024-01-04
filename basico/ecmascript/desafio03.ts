9f6472e482374850c91fe68c30c984c2f3bc9005
futuroDevLeo <leonunes07@outlook.com>
2024-01-04 18:07:41 -0300

    feat: resolved activity desafio03

--- basico/ecmascript/desafio03.ts
+++ basico/ecmascript/desafio03.ts
@@ -1,36 +1,31 @@
 // Exercicio 1
-var dobro = function (valor) {
+var dobro = function (valor: number): number {
     return valor * 2
-}
-console.log(dobro(10))
+};
+console.log(dobro(10));
 
 // Exercicio 2
-var dizerOla = function (nome) {
-    if (nome === undefined) { nome = "Pessoa" }
+const dizerOla = function (nome: string = "Pessoa"): void {
     console.log("Ola, " + nome)
-}
-
-dizerOla()
-dizerOla("Anna")
+};
+dizerOla();
+dizerOla("Anna");
 
 // Exercicio 3
-var nums = [-3, 33, 38, 5]
-console.log('???')
+const nums = [-3, 33, 38, 5];
+console.log(Math.min(...nums));
 
 // Exercicio 4
-var array = [55, 20]
-
-console.log(array)
+const array = [55, 20];
+array.push(...nums);
+console.log(array);
 
 // Exercicio 5
-var notas = [8.5, 6.3, 9.4]
-var notas1 = notas[0]
-var notas2 = notas[1]
-var notas3 = notas[2]
-console.log(nota1, nota2, nota3)
+const notas = [8.5, 6.3, 9.4];
+const [nota1, nota2, nota3] = notas;
+console.log(nota1, nota2, nota3);
 
 // Exercicio 6
-var cientista = { primeiroNome: "Will", experiencia: 12 }
-var primeiroNome = cientista.primeiroNome
-var experiencia = cientista.experiencia
-console.log(primeiroNome, experiencia)
+const cientista = { primeiroNome: "Will", experiencia: 12 };
+const { primeiroNome, experiencia } = cientista;
+console.log(primeiroNome, experiencia);
