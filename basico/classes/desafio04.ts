f7c23369d8110c2e8077b39e3789b47dd8df1a2a
futuroDevLeo <leonunes07@outlook.com>
2024-01-06 15:54:35 -0300

    feat: new challeng resolved

--- basico/classes/desafio04.ts
+++ basico/classes/desafio04.ts
@@ -18,3 +18,34 @@ const produto2 = new Produto('Espelho', 9900, 0.15);
 
 console.log(produto1);
 console.log(produto2);
+
+
+// Criar método preço com desconto
+// Quais são os parametros e o retorno?
+// Alterar método resumo para mostrar preço com desconto
+
+class ModeloProduto {
+    constructor(
+        public nome: string,
+        public preco: number,
+        public desconto: number = 0
+    ) {
+
+    }
+
+    public resumo(): string {
+        const precoAtual: number = this.precoComDesconto(this.preco, this.desconto);
+        return this.desconto > 0 ? `${this.nome} custa R$${precoAtual.toFixed(2)} (${this.desconto * 100}% off)`
+            : `${this.nome} custa R$${precoAtual.toFixed(2)}`
+    }
+
+    public precoComDesconto(preco: number, desconto: number): number {
+        return preco / 100 - (preco / 100) * desconto
+    }
+};
+
+const produto3 = new ModeloProduto('Caneta Bic', 100);
+const produto4 = new ModeloProduto('Caixa de Papel Cartão', 4999, 0.15);
+
+console.log(produto3.resumo());
+console.log(produto4.resumo());
