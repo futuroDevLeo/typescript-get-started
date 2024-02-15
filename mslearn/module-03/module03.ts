d49b3d47840f970ae7dae02b20d148755cf07b1a
futuroDevLeo <leonunes07@outlook.com>
2024-02-15 16:04:11 -0300

    feat: more ways to use interfaces in typescript

--- mslearn/module-03/module03.ts
+++ mslearn/module-03/module03.ts
@@ -11,7 +11,7 @@ const myIceCream: IceCream = {
 
 console.log(myIceCream.flavor);
 
-function tooManyScoops(dessert: IceCream) {
+function tooManyScoops(dessert: IceCream | Sundae) {
     if (dessert.scoops >= 4) {
         return dessert.scoops + ' is too many scoops!';
     } else {
@@ -20,3 +20,28 @@ function tooManyScoops(dessert: IceCream) {
 }
 
 console.log(tooManyScoops({ flavor: 'Vanilla', scoops: 5 }));
+
+interface Sundae extends IceCream {
+    sauce: 'cocholate' | 'caramel' | 'strawberry';
+    nuts?: boolean;
+    whippedCream?: boolean;
+    instructions?: string;
+}
+
+let myNewIceCream: Sundae = {
+    flavor: 'vanilla',
+    scoops: 2,
+    sauce: 'caramel',
+    nuts: true
+}
+
+console.log(tooManyScoops({ flavor: 'vanilla', scoops: 5, sauce: 'caramel' }));
+
+interface IceCreamArray {
+    [index: number]: string;
+}
+
+let myIceCreamArray: IceCreamArray;
+myIceCreamArray = ['chocolate', 'vanilla', 'strawberry'];
+let myStr: string = myIceCreamArray[0];
+console.log(myStr);
